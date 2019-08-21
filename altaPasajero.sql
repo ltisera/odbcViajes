6516b225eb5d898a62f2803f0f76e3ddcc456624
ltisera <lucastisera@gmail.com>
2019-08-21 18:19:42 -0300

    Limpieza de master
    Creado el branch ideas

--- altaPasajero.sql
+++
@@ -1,5 +0,0 @@
-create procedure altaPasajero (in dni int, in nombre varchar(45), in apellido varchar(45), in telefono varchar(45), in email varchar(45), in millas float, in clave varchar(45), in direccion varchar(45), in nacionalidad varchar(45))
-BEGIN
-	insert into pasajero (dni,nombre,apellido,telefono,email,millas,clave,direccion,nacionalidad) 
-    values(dni,nombre,apellido,telefono,email,millas,clave,direccion,nacionalidad);
-END
