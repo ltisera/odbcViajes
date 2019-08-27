3173d49faf52583b1122a27d8f572b5f1a6ea0ec
cmathov <31076435+cmathov@users.noreply.github.com>
2019-08-27 14:56:20 -0300

    ciudadDAO

---
+++ DAO/ciudadDAO.py
@@ -0,0 +1,85 @@
+from ciudad import Ciudad
+import mysql.connector
+from mysql.connector import Error
+from ConexionBD import ConexionBD
+
+class CiudadDAO(ConexionBD):
+    def __init__(self):
+        pass
+
+    def agregarCiudad(self, ciudad):
+        valido = False
+        try:
+            self.crearConexion()
+            self._micur.callproc("altaCiudad",ciudad.datos())
+            self._bd.commit()
+            valido = True
+            
+        except mysql.connector.errors.IntegrityError as e:
+            print(e)
+            
+        finally:
+            self.cerrarConexion()
+
+        return valido
+
+    def eliminarCiudad(self, id):
+        valido = False
+        try:
+            self.crearConexion()
+            print("TST")
+            self._micur.callproc("bajaCiudad", (id,))
+            self._bd.commit()
+            valido = True
+            
+        except mysql.connector.Error as err:
+            print("DANGER ALGO OCURRIO: " + str (err))
+            
+        finally:
+            self.cerrarConexion()
+
+        return valido
+    
+    def traerCiudad(self, id):
+        cTraido = None
+        try:
+            self.crearConexion()
+            self._micur.callproc("consultaCiudad", (id,))
+            for res in self._micur.stored_results():
+                r = res.fetchone()
+                if(r is not None):
+                    cTraido = Pasaje(registro = r)
+
+        except mysql.connector.Error as err:
+            print("DANGER ALGO OCURRIO: " + str (err))
+
+        finally:
+            self.cerrarConexion()
+
+        return cTraido
+
+    def traerCiudades(self):
+        lstCiudades = None
+        try:
+            self.crearConexion()
+            self._micur.callproc("consultaCiudades")
+            for result in self._micur.stored_results():
+                reg = result.fetchall()
+                if reg is not None:
+                    for r in reg:
+                        lstCiudades.append(Ciudad(r))
+                else:
+                    print("No hay ciudades")
+
+                
+        except mysql.connector.Error as err:
+            print("DANGER ALGO OCURRIO: " + str (err))
+
+        finally:
+            self.cerrarConexion()
+
+        return lstCiudades
+
+
+if __name__ == '__main__':
+    
