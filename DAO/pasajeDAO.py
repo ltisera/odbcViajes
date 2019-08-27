1517b3cddb20f82f44cd840ecccea01b5c162d54
ltisera <lucastisera@gmail.com>
2019-08-26 21:50:49 -0300

    Pasajero, PasajeroDAO y Store Procedures

---
+++ DAO/pasajeDAO.py
@@ -0,0 +1,92 @@
+from pasaje import Pasaje
+import mysql.connector
+from mysql.connector import Error
+from ConexionBD import ConexionBD
+
+class PasajeDAO(ConexionBD):
+    def __init__(self):
+        pass
+
+    def agregarPasaje(self, pasaje):
+        valido = False
+        try:
+            self.crearConexion()
+            print("TST")
+            self._micur.callproc("altaPasaje", ('1', '2019-06-15', 12.5, 3, 1, 123, 'dinero'))
+            print("NO Commit")
+            self._bd.commit()
+            valido = True
+            
+        except mysql.connector.errors.IntegrityError as e:
+            pass
+            
+        finally:
+
+
+            self.cerrarConexion()
+
+        return valido
+    
+    def traerPasaje(self, DNI):
+        try:
+            pTraido = None
+            self.crearConexion()
+            self._micur.callproc("consultaPasaje", (DNI,))
+            for res in self._micur.stored_results():
+                r = res.fetchone()
+                if(r is not None):
+                    pTraido = Pasaje(registro = r)
+            return pTraido
+
+        except mysql.connector.Error as err:
+            print("DANGER ALGO OCURRIO: " + str (err))
+        finally:
+            self.cerrarConexion()
+    
+    def traerPasajesXMail(self, mail):
+        try:
+            self.crearConexion()
+            self._micur.callproc("consultaPasajeXMail", (mail,))
+            for result in self._micur.stored_results():
+                return result.fetchone()
+                
+        except mysql.connector.Error as err:
+            print("DANGER ALGO OCURRIO: " + str (err))
+        finally:
+            self.cerrarConexion()
+    def loginPasaje(self, dni, clave):
+        try:
+            if (self.traerPasaje(dni).clave == clave):
+                print("Pasaje registrado")
+                return True
+            else:
+                return False
+        except AttributeError as e:
+            print ("Excep")
+            return False
+
+"""
+    Implementar el metodo Mathov    
+    def loginPasaje(dni, mail = "", clave):
+        self.traerPasaje(dni)
+"""
+
+
+if __name__ == '__main__':
+    tstPDAO = PasajeDAO()
+    tpasaje = Pasaje()
+    print("ta")
+    tpasaje.codigo = "1"
+    print("ta")
+    tpasaje.fecha = "2019-06-15"
+    tpasaje.valor = 12.5
+    tpasaje.pasajero = 3
+    tpasaje.origen = 1
+    tpasaje.destino = 123
+    tpasaje.formaPago = "dinero"
+    print("Mi pasaje")
+    print(tpasaje.datos())
+    print("PERSIS")
+    tstPDAO.agregarPasaje(tpasaje)
+    print("Vamo")
+    
--- StoreProcedures.sql
+++ StoreProcedures.sql
@@ -1,22 +1,22 @@
 delimiter //
 
-DROP PROCEDURE IF exists altaPasajero;
+DROP PROCEDURE altaPasajero;
 CREATE PROCEDURE altaPasajero (in dni int, in nombre varchar(45), in apellido varchar(45), in telefono varchar(45), in email varchar(45), in millas float, in clave varchar(45), in direccion varchar(45), in nacionalidad varchar(45))
 BEGIN
 	insert into pasajero (dni,nombre,apellido,telefono,email,millas,clave,direccion,nacionalidad) 
     values(dni,nombre,apellido,telefono,email,millas,clave,direccion,nacionalidad);
 END//
 
-DROP PROCEDURE IF exists consultaPasajero;
+DROP PROCEDURE consultaPasajero;
 CREATE PROCEDURE consultaPasajero(in DNI int)
 BEGIN
 	SELECT * FROM pasajero where pasajero.DNI = DNI;
 END//
 
-DROP PROCEDURE IF exists altaPasaje;
-CREATE PROCEDURE altaPasaje (in codigo varchar(10), in fecha date, in valor float , in pasajero int, in origen int, in destino int, in formaPago enum)
+DROP PROCEDURE altaPasaje;
+CREATE PROCEDURE altaPasaje (in codigo varchar(10), in fecha date, in valor float , in pasajero int, in origen int, in destino int, in formaPago enum("dinero","millas"))
 BEGIN
-	insert into pasajero (codigo, fecha, valor,  pasajero, origen,destino, formaPago) 
+	insert into pasaje (codigo, fecha, valor,  pasajero, origen,destino, formaPago) 
     values(codigo, fecha, valor,  pasajero, origen,destino, formaPago);
 END//
 delimiter ;
